16eabca340574e80add4a40fe9fbc852cc0aca95
flyjp9512 <flyjp@qq.com>
2019-03-09 14:18:08 +0800

    通道之间的数据传输与内存映射文件

--- src/com/flyjp/nio/TestChannel.java
+++ src/com/flyjp/nio/TestChannel.java
@@ -2,10 +2,7 @@ package com.flyjp.nio;
 
 import org.junit.Test;
 
-import java.io.FileInputStream;
-import java.io.FileNotFoundException;
-import java.io.FileOutputStream;
-import java.io.IOException;
+import java.io.*;
 import java.nio.ByteBuffer;
 import java.nio.MappedByteBuffer;
 import java.nio.channels.FileChannel;
@@ -32,8 +29,51 @@ import java.nio.file.StandardOpenOption;
  *      DatagramSocket
  *   2.在JDK1.7中的NIO.2针对各个通道提供了静态方法open()
  *   3.在JDk1.7中的NIo.2的Files工具类的newByteChannel()
+ *
+ *  四，分散（Scatter）与聚集（Gather）
+ *   分散读取（Scattering Reads）:将通道中的数据分散到多个缓冲区中
+ *   剧集写入（Gathering Write）:将多个缓冲区的数据聚集到通道中
  */
 public class TestChannel {
+
+    //分散和聚集
+    @Test
+    public void test4(){
+        RandomAccessFile raf1 = null;
+        RandomAccessFile raf2 = null;
+        FileChannel channel1 = null;
+        FileChannel channel2 = null;
+        try{
+            raf1 = new RandomAccessFile("1.jpg","rw");
+
+            //1.获取通道
+            channel1 = raf1.getChannel();
+
+            //2.分配指定大小的缓冲区
+            ByteBuffer buf1 = ByteBuffer.allocate(100);
+            ByteBuffer buf2 = ByteBuffer.allocate(1024);
+
+            //3.分散读取
+            ByteBuffer[] bufs = {buf1,buf2};
+            channel1.read(bufs);
+
+            for(ByteBuffer byteBuffer :bufs){
+                byteBuffer.flip();
+            }
+            System.out.println(new String(bufs[0].array(),0,bufs[0].limit()));
+            System.out.println("----------");
+            System.out.println(new String(bufs[1].array(),0,bufs[1].limit()));
+
+            //聚集写入
+            raf2 = new RandomAccessFile("4.jpg","rw");
+            channel2 = raf2.getChannel();
+            channel2.write(bufs);
+
+        }catch (IOException e){
+            e.printStackTrace();
+        }
+
+    }
 
     //通道之间的数据传输(直接缓冲区)
     @Test
